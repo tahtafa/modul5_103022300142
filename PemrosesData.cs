7bfe6b3ceaff1112d5f88a7bd260ef2a882108d0
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-19 16:29:23 +0700

    menambahkan class PemrosesData dengan method DapatkanNilaiTerbesar

---
+++ PemrosesData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_103022300142
+{
+    class PemrosesData
+    {
+
+        public void DapatkanNilaiTerbesar<T>(T data1, T data2, T data3)
+        {
+            dynamic temp1 = data1;
+            dynamic temp2 = data2;
+            dynamic temp3 = data3;
+            dynamic temp1maks = temp1;
+
+
+            if (temp1maks < temp2)
+            {
+                temp1maks = temp2;
+                Console.WriteLine("Nilai terbesar adalah " + temp2);
+            }
+            else if (temp3 > temp1maks)
+            {
+                temp1maks = temp3;
+                Console.WriteLine("Nilai terbesar adalah " + temp3);
+            }
+            else {
+                Console.WriteLine("nilai terbesar adalah " + temp1maks);
+            }
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            //first
+            float input1 = 10;
+            float input2 = 30;
+            float input3 = 22;
+
+            PemrosesData pemrosesData = new PemrosesData();
+            pemrosesData.DapatkanNilaiTerbesar(input1, input2, input3);
         }
     }
 }
